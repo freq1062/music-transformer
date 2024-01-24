f8909801a1dcd23412b573e7575b2b9e7169a9ff
freq1062 <128990428+freq1062@users.noreply.github.com>
2024-01-24 17:20:49 -0500

    Update train.py

--- train.py
+++ train.py
@@ -13,8 +13,8 @@ from pathlib import Path
 import numpy as np
 
 from torch.utils.tensorboard import SummaryWriter
+from torchinfo import summary
 
-configure.run() #Music21 visualization
 import tensorflow as tf
 import glob
 import pickle
@@ -75,7 +75,8 @@ def buildVocab(words,specialTokens=[]): #From a list of words create a dictionar
 
 def createDataset(filenames, pkl:bool=True): #Recheck this
     if pkl: #load from pkl files
-        with open(filenames[0], 'rb') as fp: ds = pickle.load(fp)[:1024] #dataset
+        num_samples = min(int(get_config()["num_samples"]), 69961)
+        with open(filenames[0], 'rb') as fp: ds = pickle.load(fp)[:num_samples] #dataset
         with open(filenames[1], 'rb') as fp: tokenizer = pickle.load(fp) #tokenizer
         return tokenizer, ds
     else:
@@ -83,7 +84,8 @@ def createDataset(filenames, pkl:bool=True): #Recheck this
         seq_len = get_config()["seq_len"]-2 #EOS, SOS tokens
         #Actually seq_len depends on what was saved to the pkl file if loading that way
         notes_raw = []
-        for f in tqdm(filenames[:],desc=f"Loading files from dataset..."): #Later: Get data from premade .pkl file
+        num_files = min(int(get_config()["num_files"]), 1282)
+        for f in tqdm(filenames[:num_files],desc=f"Loading files from dataset..."): #Later: Get data from premade .pkl file
             #Combine all of the notes from files into 1 array ["note1", "note2", "note3"...]
             fileNotes = midi_to_events(f)
             splitNotes = [fileNotes[i:i + seq_len] for i in range(0, len(fileNotes), seq_len)]
@@ -141,7 +143,6 @@ def top_k_decode(model, input, config, device, k=3):
     return output.squeeze(0)
 
 #
-
 def run_validation(model, validation_ds, device, print_msg, num_examples:int=2):
     model.eval()
     count = 0
@@ -177,32 +178,48 @@ def run_validation(model, validation_ds, device, print_msg, num_examples:int=2):
         
             if count == num_examples:
                 break
-            
 
-def arrangeFile(filename): #Arrange an input midi file to the same format as the one the model recognizes
-    ds = []
+def split_array_at_count(arr, target_count, target_element):
+    count_a = 0
+    split_index = 0
+
+    for i, element in enumerate(arr):
+        if element == target_element:
+            count_a += 1
+
+        if count_a == target_count:
+            split_index = i + 1
+            break
+
+    return arr[:split_index]
+
+def arrangeSequence(filename, notes:int, start:int):
     seq_len = get_config()["seq_len"]-2
-    notes_raw = []
-    fileNotes = midi_to_events(filename)
-    
-    notes_raw = list(map(lambda x: tokenizer[x], fileNotes)) #Tokenize everything
-    notes_raw = np.split(notes_raw, np.arange(seq_len, len(notes_raw), seq_len)) #[[seq_len], [seq_len], ...]
+    fileNotes = midi_to_events(filename)[start:]
+    #Return the sequence with the number of notes, but ignore timesteps
+    notecount = 0
+    for i, element in enumerate(fileNotes):
+        if str(element).startswith("t") == False: #Not a timestep so must be a note
+            notecount+=1
+        if notecount >= notes:
+            fileNotes = fileNotes[:(i+1)]
+            break
+    input = list(map(lambda x: tokenizer[x], fileNotes))
 
     #Create special tokens for padding
     sos_token = torch.tensor([tokenizer["EOS"]], dtype=torch.int64)
     eos_token = torch.tensor([tokenizer["SOS"]], dtype=torch.int64)
     pad_token = torch.tensor([tokenizer["PAD"]], dtype=torch.int64)
 
-    for seq in notes_raw:
-        num_padding_tokens = seq_len - len(seq)
-        input = torch.cat([
-            sos_token, #1 additional
-            torch.tensor(seq, dtype=torch.int64),
-            eos_token, #2 additional
-            torch.tensor([pad_token] * num_padding_tokens, dtype=torch.int64),])
-        #encoder_mask = (encoder_input != pad_token).unsqueeze(0).unsqueeze(0).int()
-        ds.append({"input":input, "src_seq":seq})
-    return ds
+    num_padding_tokens = seq_len - len(input)
+    input = torch.cat([
+        sos_token, #1 additional
+        torch.tensor(input, dtype=torch.int64),
+        eos_token, #2 additional
+        torch.tensor([pad_token] * num_padding_tokens, dtype=torch.int64),])
+    #encoder_mask = (encoder_input != pad_token).unsqueeze(0).unsqueeze(0).int()
+    return {"input":input, "src_seq":fileNotes}
+
 
 def nObj(noteData):
     return music21.note.Note(music21.pitch.Pitch(noteData[0]), type=durationCodes[noteData[1]])
@@ -285,7 +302,10 @@ def get_ds(config):
     return train_dataloader, val_dataloader #Returns formatted tensors
 
 def get_model(config, vocab_size): 
-    model = torch.jit.script(MusicTransformer(vocab_size, config["d_model"], 8))
+    #torch jit version: 64% faster
+    #model = torch.jit.script(MusicTransformer(vocab_size, config["d_model"], 8))
+    #Standard version
+    model = MusicTransformer(vocab_size, config["d_model"], 8)
     #Actually seq_len doesn't really matter because it's all done in preprocessing
     #Currently d_model is 512, num_heads is 8 according to original transformer paper
     #Leaving depth, d_ff, dropout to default 
@@ -314,14 +334,14 @@ def train_model(config):
     model_filename = latest_weights_file_path(config) if preload == 'latest' else get_weights_file_path(config, preload) if preload else None
     if model_filename:
         print(f'Preloading model {model_filename}')
-        state = torch.load(model_filename)
+        state = torch.load(model_filename, map_location=device)
         model.load_state_dict(state['model_state_dict']) 
         initial_epoch = state['epoch'] + 1
         optimizer.load_state_dict(state['optimizer_state_dict'])
         global_step = state['global_step']
     else:
         print('No model to preload, starting from scratch')
-    
+
     #Training loop
     for epoch in range(initial_epoch, config["num_epochs"]):
         torch.cuda.empty_cache()
@@ -330,13 +350,13 @@ def train_model(config):
         for batch in batch_iterator:
             inputs = []
             labels = []
-
             for pair in batch:
                 inputs.append(pair["input"].to(device)) #(seq_len)
                 labels.append(pair["label"].to(device)) #(batch, seq_len)
 
             inputs = torch.stack(inputs)
             labels = torch.stack(labels)
+            #summary(model, input_data=inputs, dtypes=[torch.long],verbose=1)
             model_output = model(inputs)
             #Calculate cross entropy loss
             loss = loss_fn(model_output.view(-1, len(list(tokenizer.keys())) ), labels.view(-1))
@@ -352,9 +372,10 @@ def train_model(config):
             global_step += 1
         
         #Run validation
-        run_validation(model, val_dataloader, device, lambda msg: batch_iterator.write(msg), 1)
+        #run_validation(model, val_dataloader, device, lambda msg: batch_iterator.write(msg), 1)
 
         model_filename = get_weights_file_path(config, f"{epoch:02d}")
+        if (delete_filename := get_weights_file_path(config, f"{epoch-3:02d}")) and pathlib.Path(delete_filename).exists(): pathlib.Path(delete_filename).unlink()
         torch.save({
             'epoch': epoch,
             'model_state_dict': model.state_dict(),
@@ -362,7 +383,7 @@ def train_model(config):
             'global_step': global_step
         }, model_filename)
 
-def getOutput(filename=str): #filename of midi file to send into transformer, mode, number of bars to generate
+def getOutput(filename:str, notes:int, start:int): #filename of midi file to send into transformer, mode, number of bars to generate
     config = get_config()
     #Get device information
     device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_built() or torch.backends.mps.is_available() else "cpu"
@@ -378,7 +399,7 @@ def getOutput(filename=str): #filename of midi file to send into transformer, mo
 
     if model_filename:
         print(f'Preloading model {model_filename}')
-        state = torch.load(model_filename)
+        state = torch.load(model_filename, map_location=device)
         model.load_state_dict(state['model_state_dict'])
         print("Successfully loaded model")
     else:
@@ -386,20 +407,22 @@ def getOutput(filename=str): #filename of midi file to send into transformer, mo
         return
     
     model.eval()
-    arranged_file = arrangeFile(filename)[0] #Only up to max seq_len for now
+    arranged_file = arrangeSequence(filename, notes, start) #Only up to max seq_len for now
     with torch.no_grad():
-        model_out = top_k_decode(model, arranged_file["input"], config, device, 3)
+        model_out = top_k_decode(model, arranged_file["input"], config, device, 5)
         #Detokenize model output
         func = np.vectorize(lambda x: list(tokenizer.keys())[x])
         model_out_text = list(filter(lambda x: x != "SOS", func(model_out.detach().cpu().numpy()).tolist())) #Turn tokens back to notes and filters out padding
-        print(model_out_text)
-        return decodeEvents(model_out_text)
+        print(" ".join(model_out_text))
+        return decodeEvents(arranged_file["src_seq"] + model_out_text)
 
-#Begin training loop
-if __name__ == '__main__':
+Begin training loop
+ if __name__ == '__main__':
     warnings.filterwarnings("ignore")
     config = get_config()    
-    train_model(config)
-        
-#filename = r""
-#getOutput(filename).show()
+     train_model(config)
+
+if __name__ == '__main__':
+    filename = r""
+    getOutput(filename, 30, 30).show("midi")
+    
